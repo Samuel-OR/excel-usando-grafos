d291890795ac41f2c9b97595f56ac35db95af623
Samuel-OR <samueloliveira0014@gmail.com>
2019-11-10 16:20:17 -0300

    Finalizado.

--- excel.c
+++ excel.c
@@ -5,7 +5,8 @@
 #include <sys/time.h>
 #define LINHAS 20
 #define COLUNAS 8
-int enderecoGERAL;
+int enderecoGERAL=0;
+int grau_max = (COLUNAS>LINHAS) ? COLUNAS:LINHAS ;
 typedef struct{
 	int linha;
 	int coluna;
@@ -13,12 +14,8 @@ typedef struct{
 
 typedef struct{
 	int nro_vertices;
-	int **arestas;
-	int eh_ponderado;
 	int **pesos;
-	int **ponteiro;
-	int *grau;
-	int grau_max;
+	int ***ponteiro;
 } Grafo;
 
 typedef struct{
@@ -33,75 +30,43 @@ typedef struct{
 }Intervalo;
 
 
-//COLETAR TEMPO EM MICROSEGUNDOS
-long getMicrotime(){
-	struct timeval currentTime;
-	gettimeofday(&currentTime, NULL);
-	return currentTime.tv_sec * (int)1e6 + currentTime.tv_usec;
-}
-
-
 //FUNÇÕES UTILIZADAS PARA GERENCIAR GRAFO
-Grafo *cria_Grafo(int nro_vertices, int grau_max, int eh_ponderado){
+Grafo *cria_Grafo(int nro_vertices){
 	Grafo *gr;
 	gr = (Grafo *)malloc(sizeof(Grafo));
 
 	if (gr != NULL){
 		int i;
 		gr->nro_vertices = nro_vertices;
-		gr->grau_max = grau_max;
-		gr->eh_ponderado = (eh_ponderado != 0) ? 1 : 0;
-		gr->grau = (int *)calloc(nro_vertices, sizeof(int));
+		gr->pesos = (int **)malloc(nro_vertices * sizeof(int *));
+		gr->ponteiro = (int ***)malloc( nro_vertices* sizeof(int **));
 
-		gr->arestas = (int **)malloc(nro_vertices * sizeof(int *));
-		for (i = 0; i < nro_vertices; i++){
-			gr->arestas[i] = (int *)malloc(grau_max * sizeof(int));
+		for (int j = 0; j < nro_vertices; j++){
+			gr->pesos[j] = (int *)malloc(grau_max * sizeof(int));
+			gr->ponteiro[j] = (int **)malloc(grau_max * sizeof(int*));
+			
+			for(int z=0; z<grau_max; z++)
+				gr->ponteiro[j][z] = &enderecoGERAL;
+			
+			//	gr->ponteiro[j][z] = -1;
 		}
-		if (gr->eh_ponderado == 1){
-			gr->pesos = (int **)malloc(nro_vertices * sizeof(int *));
-			gr->ponteiro = (int **)malloc( LINHAS*COLUNAS* sizeof(int *));
-
-			for (int j = 0; j < nro_vertices; j++){
-				gr->pesos[j] = (int *)malloc(grau_max * sizeof(int));
-				gr->ponteiro[j] = &enderecoGERAL;
-				
-				//for(int z=0; z<grau_max; z++)
-				//	gr->ponteiro[j][z] = -1;
-			}
-		}
+		
 	}
 	return gr;
 }
-int insereAresta(Grafo *gr, int linha, int coluna, int eh_digrafo, float peso){
-	int resul = 0;
-	if (gr != NULL){
-		if ((linha >= 0 && linha < gr->nro_vertices) || (coluna >= 0 && coluna < gr->nro_vertices)){
-			gr->arestas[linha][gr->grau[linha]] = coluna;
-			if (gr->eh_ponderado)
-				gr->pesos[linha][gr->grau[linha]] = peso;
-			gr->grau[linha]++;
-			if (eh_digrafo == 0)
-				insereAresta(gr, coluna, linha, 1, peso);
-			resul = 1;
-		}
-	}
-	return resul;
-}
 void liberar_Grafo(Grafo *gr){
 	if (gr != NULL){
-		int i;
-		for (i = 0; i < gr->nro_vertices; i++)
-			free(gr->arestas[i]);
-		free(gr->arestas);
-
-		if (gr->eh_ponderado == 1){
-			for (i = 0; i < gr->nro_vertices; i++)
-				free(gr->pesos[i]);
-			free(gr->pesos);
+		for (int i = 0; i < gr->nro_vertices; i++){
+			for(int z=0; z < grau_max; z++)
+				free(gr->ponteiro[i][z]);
+			free(gr->ponteiro[i]);
+			free(gr->pesos[i]);
 		}
-		free(gr->grau);
+		free(gr->pesos);
+		free(gr->ponteiro);
 		free(gr);
 	}
+	printf("GRAFO LIBERADO\n");
 }
 
 //FUNÇÕES PEGAR COLUNA/LINHA/TIPO DE OPERAÇÃO/INTERVALO
@@ -266,20 +231,20 @@ int getReferencia(Grafo *gr,char *comando){
 	
 	colunB = comando[x++]-65;
 
-	if(comando[x+1] != '\0'){
+	if(comando[x+1]>=30 && comando[x+1]<=39){
 		letra[0] = comando[x];
 		letra[1] = comando[x+1];
 		lineB = atoi(letra);
 	}else{
-		letra[0] = comando[x];
+		letra[0] = ' ';
+		letra[1] = comando[x];
 		lineB = atoi(letra);
 	}
 
-	gr->ponteiro[(lineA+1)*(colunA+1)] = &(gr->pesos[lineB][colunB]);
-
-	printf("TEM QUE REFERENCIA\n");
+	gr->ponteiro[lineA][colunA] = &(gr->pesos[lineB][colunB]);
 }
 
+//FUNÇÕES PARA TRATAR ENTRADA
 int setDADO(Grafo *gr, char *comando, int *referenciaCelula){
 	int resultado=0;
 	int op = getFunction(comando);
@@ -302,8 +267,6 @@ int setDADO(Grafo *gr, char *comando, int *referenciaCelula){
 	}
 	return resultado;
 }
-
-
 void processarEntrada(Grafo *gr, char *comando){
 	int auxReferencia=0;
 	int valorCelula = setDADO(gr, comando, &auxReferencia);
@@ -312,29 +275,23 @@ void processarEntrada(Grafo *gr, char *comando){
 
 	if(auxReferencia == 0){
 		gr->pesos[line][colun] = valorCelula;
-		gr->ponteiro[(line+1)+(colun+1)] = &enderecoGERAL;
-
+		gr->ponteiro[line][colun] = &enderecoGERAL;
 	}else{
 		getReferencia(gr,comando);
-		printf("Sei Não\n");
 	}
-
-
 }
 
+
+//FUNÇÃO PARA MOSTRAR TABELA
 void mostrarPesos(Grafo *gr){
 	for (int i = 0; i < LINHAS; i++){
-		if(i<10-1)
-			printf(" %d | ",i+1 );
-		else printf("%d | ",i+1 );
-		
+		printf("%2d |",i+1 );
 		for(int x=0; x< COLUNAS; x++){
-			if(gr->ponteiro[(i+1)*(x+1)]== &enderecoGERAL)
-				printf("%d ",gr->pesos[i][x]);
-			else{
-				printf("%d ",*(gr->ponteiro[(i+1)*(x+1)]));
+			if(gr->ponteiro[i][x] == &enderecoGERAL){
+				printf("%7d|",gr->pesos[i][x]);
+			}else{
+				printf("%7d|",*(gr->ponteiro[i][x]));
 			}
-
 		}
 		printf("\n");
 	}
@@ -345,35 +302,31 @@ int main(){
 	int op;
 	char entrada[100];
 	Grafo *gr = NULL;
-	gr = cria_Grafo(LINHAS+COLUNAS, LINHAS, 1);
+	gr = cria_Grafo(LINHAS+COLUNAS);
+
 	processarEntrada(gr, "A1 10\0");
 	processarEntrada(gr, "B1 20\0");
 	processarEntrada(gr, "A2 30\0");
 	processarEntrada(gr, "C1 @soma(A1..B2)\0");
+	processarEntrada(gr, "D1 =C1\0");
 	while (0==0){
 		printf("\n--------- MENU ---------\n");
 		printf(" [1]-Reiniciar Tabela\n");
 		printf(" [2]-Inserir Comando\n");
 		printf(" [3]-Mostrar Tabela de dados\n");
-		printf(" [4]-Liberar\n");
 		printf(" [0]-SAIR\n");
 		printf("----------------------\n");
 		printf("Escolha uma opção: ");
 		scanf("%d", &op);
 		switch (op){
 			case 1:
+				
 				if (gr != NULL)
 					liberar_Grafo(gr);
-				//gr = cria_Grafo(nro_vertices+1, grau_max, eh_ponderado);
-				gr = cria_Grafo(LINHAS+COLUNAS, LINHAS, 1);
-				printf("\n- - Grafo CRIADO - -\n");
+				gr = cria_Grafo(LINHAS+COLUNAS);
 
-				for(int line = 0; line<LINHAS; line++){
-					for(int colun=LINHAS; colun< (LINHAS+COLUNAS); colun++){
-						insereAresta(gr, line, colun,0, 0);
-					}
-				}
-				printf("\n- - ARESTAS inseridas - -\n");
+				printf("\n- - Grafo CRIADO - -\n");
+				
 				break;
 			case 2:
 				setbuf(stdin, NULL);
@@ -384,28 +337,13 @@ int main(){
 
 			case 3:
 				printf("\n- - TABELA DADOS - -\n");
+				printf("___|___A___|___B___|___C___|___D___|___E___|___F___|___G___|___H___|\n");
 				mostrarPesos(gr);
-				break;
-			case 4:
-				if (gr != NULL)
-					liberar_Grafo(gr);
-				printf("\n- - Grafo LIBERADO - -\n");
 				break;
 			case 0:
 				exit(0);
 				break;
 		}
 	}
-
-	
-	
-
-	/*
-	int *aux;
-	getCelula("A5");
-	setDADO("A1 ", aux);
-	*/
-
-
 	return 0;
 }
